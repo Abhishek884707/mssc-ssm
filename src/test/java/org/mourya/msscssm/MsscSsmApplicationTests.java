0e9dbaa8e98f1d41a1ab6f29942c0aebcb3ab995
Abhishek Kumar <2021mt70042@wilp.bits-pilani.ac.in>
2024-01-05 13:23:07 +0530

    initial commit

---
+++ src/test/java/org/mourya/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mourya.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
